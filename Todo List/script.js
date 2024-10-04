70fb08c7e89e65cf818a2868468d8a157e1a8302
ClaraMalatsi <142051594+ClaraMalatsi@users.noreply.github.com>
2024-10-04 12:08:50 +0200

    script.js

--- Todo List/script.js
+++ Todo List/script.js
@@ -75,11 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
             renderTasks();
         }
         }
-    /*
-
-   bgColorButton.onclick = () => {
-        document.body.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 75%)`;
-    };*/
+    
 
     showAllButton.onclick = renderTasks;
     showActiveButton.onclick = () => {
@@ -186,20 +182,7 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.style.backgroundColor = getRandomColor();
     });
 
-    /*function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
-    renderTasks();
-});
-*/
-
-// Existing addTask function and other code remains the same
+    
 document.getElementById('addButton').addEventListener('click', addTask);
 
 function addTask() {
